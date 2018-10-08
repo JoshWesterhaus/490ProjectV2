4e7a8e0d700b2baa25994f317ac9701269a9c07e
Steven Burnham <42622880+slburnham@users.noreply.github.com>
2018-10-08 16:15:34 -0400

    Add files via upload

--- src/Connection.java
+++ src/Connection.java
@@ -1,8 +1,6 @@
 import java.sql.*;
 import java.util.ArrayList;
 
-import javafx.stage.Stage;
-
 /*
  * Class to connect to the MySQL Database 
  * Similar to the facade class that has a singleton connection that is established 
@@ -30,6 +28,7 @@ public class Connection {
 				Login tempLogin = new Login(userName, password, uid);
 				userArray.add(tempLogin);
 			}
+			System.out.println(userArray);
 			return userArray;
 		} catch (SQLException error) {
 			System.out.println(error.getMessage());
@@ -37,32 +36,32 @@ public class Connection {
 		}
 	}
 	
-	//public static ArrayList<TeeTime> getTeeTimes() {
-		//try {
-			//String host = "jdbc:mysql://sql9.freesqldatabase.com/sql9255339";
-			//String dbuser = "sql9255339";
-			//String dbpass = "S8EkeFyZuD";
-			//java.sql.Connection con = DriverManager.getConnection(host, dbuser, dbpass);
+	public static ArrayList<Login> getTeeTimes() {
+		try {
+			String host = "jdbc:mysql://sql9.freesqldatabase.com/sql9255339";
+			String dbuser = "sql9255339";
+			String dbpass = "S8EkeFyZuD";
+			java.sql.Connection con = DriverManager.getConnection(host, dbuser, dbpass);
 
-			// Select tee times from database
-			//Statement test = con.createStatement();
-			//ResultSet rs = test.executeQuery("select * from teetime");
-			// Create an array of all usernames
-			//ArrayList<Login> teeTimeArray = new ArrayList<>();
-			//while (rs.next()) {
-
-				//String userName = rs.getString("username");
-				//String password = rs.getString("password");
-				//String uid = rs.getString("uid");
-				//Login tempLogin = new Login(userName, password, uid);
-				//teeTimeArray.add(tempLogin);
-			//}
-			//System.out.println(teeTimeArray);
-			//return teeTimeArray;
-		//} catch (SQLException error) {
-			//System.out.println(error.getMessage());
-			//return null;
-		//}
-	//}
+			//Select tee times from database
+			Statement test = con.createStatement();
+			ResultSet rs = test.executeQuery("select * from teetime");
+			//Create an array of all tee times
+			ArrayList<Login> teeTimeArray = new ArrayList<>();
+			while (rs.next()) {
+				String uid = rs.getString("uid");
+				String name = rs.getString("name");
+				int golfers = rs.getInt("golfers");
+				int time = rs.getInt("time");
+				String rate = rs.getString("rate");
+				int day = rs.getInt("day");
+			}
+			System.out.println(teeTimeArray);
+			return teeTimeArray;
+		} catch (SQLException error) {
+			System.out.println(error.getMessage());
+			return null;
+		}
+	}
 }
 // End class
